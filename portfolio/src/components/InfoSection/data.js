1932822c291f3d962ef152319d1dccdd799570bb
Anish Gyawali <anish.gyawali@selu.edu>
2020-12-31 12:05:45 -0600

    corrected some misspelled words.

--- portfolio/src/components/InfoSection/data.js
+++ portfolio/src/components/InfoSection/data.js
@@ -7,11 +7,10 @@ export const homeObjectOne = {
   connect: "Connect with me:",
   description: "Major: Computer Science (Scientific concentartion)",
   information:
-    "I am in senior year major in computer science from Southeastern Louisiana University. I have had opportunities to work with clients developing websites and I have grown a passion for it. Aside from my passion in programming, I alsolike reading books, travelling and Listining music.",
+    "I am in senior year major in computer science from Southeastern Louisiana University. I have had opportunities to work with clients developing websites and I have grown a passion for it. Aside from my passion in programming, I also like reading books, travelling and Listining music.",
   imgStart: false,
   //we will add image here
   dark: false,
   primary: false,
   darkText: true,
 };
-
