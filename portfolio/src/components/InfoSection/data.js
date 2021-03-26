33341f5e2072c733acb90b59108407b8811f2e0a
Anish Gyawali <anish.gyawali@selu.edu>
2021-03-25 23:11:41 -0500

    edited

--- portfolio/src/components/InfoSection/data.js
+++ portfolio/src/components/InfoSection/data.js
@@ -5,7 +5,7 @@ export const homeObjectOne = {
   lightTextDesc: false,
   topLine: "Anish Gyawali",
   connect: "Connect with me:",
-  description: "Major: Computer Science (Scientific concentartion)",
+  description: "Major: Computer Science (Scientific concentration)",
   information:
     "I am in senior year major in computer science from Southeastern Louisiana University. I have had opportunities to work with clients developing websites and I have grown a passion for it. Aside from my passion in programming, I also like reading books, travelling and Listining music.",
   imgStart: false,
